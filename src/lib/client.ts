fe394251875167828a5d08ac795bab045d985a94
Vaibhav <gvaibhav864@gmail.com>
2025-12-18 00:29:38 +0530

    vercel url added

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,11 +2,9 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (typeof window !== "undefined") return "/";
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return "http://localhost:3000";
 };
 
 export const client = treaty<App>(getBaseUrl()).api;
-
-
