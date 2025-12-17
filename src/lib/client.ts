a5e65db0e04c5446d7b78892f37caee284337b2c
Vaibhav <gvaibhav864@gmail.com>
2025-12-18 00:32:52 +0530

    vercel url added

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,7 +2,7 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return "/";
+  if (typeof window !== "undefined") return window.location.origin;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return "http://localhost:3000";
 };
